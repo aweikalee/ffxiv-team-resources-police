b262db7a5b9036ad622aa1bbde48b7dc5a1e848f
aweikalee <aweikalee@163.com>
2022-06-06 00:09:48 +0800

    refactor: combat 中副本区域名称 改由 ChangeZone 提供

--- src/store/combat.ts
+++ src/store/combat.ts
@@ -24,25 +24,28 @@ export function useCombat() {
     state.isActive = data.isActive === 'true' || data.isActive === true
 
     if (Encounter) {
-      if (Encounter.CurrentZoneName || state.duration) {
-        state.zoneName = Encounter.CurrentZoneName
+      if (state.duration) {
         state.duration = Encounter.duration
         lastEncounter = Encounter
       } else if (lastEncounter) {
-        state.zoneName = lastEncounter.CurrentZoneName
         state.duration = lastEncounter.duration
       }
     } else {
-      state.zoneName = ''
       state.duration = ''
     }
   }
 
+  function updateZoneName(data: any) {
+    state.zoneName = data.zoneName
+  }
+
   onMounted(() => {
     window.addOverlayListener('CombatData', update)
+    window.addOverlayListener('ChangeZone', updateZoneName)
   })
   onUnmounted(() => {
     window.removeOverlayListener('CombatData', update)
+    window.removeOverlayListener('ChangeZone', updateZoneName)
   })
 
   return state
